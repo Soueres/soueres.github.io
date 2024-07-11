998d1eec5e39acedd7ed367990da38fda24fcaf9
soueres <bertrand.soueres@gmail.com>
2024-07-11 18:42:14 +0200

    Text size adapatation when resizing
    only on the first button...

--- scripts/script_test.js
+++ scripts/script_test.js
@@ -4,6 +4,7 @@ window.onload = displayWindowSize;
 function displayWindowSize() {
     myWidth = window.innerWidth;
     myHeight = window.innerHeight;
+    document.getElementById("btt").style.fontSize = Math.min(Math.max(1000/myWidth, 1), 1.7) + "rem";
+    document.getElementById("top_info").innerHTML = "Width : " + myWidth + " Height : " + myHeight;
     document.getElementById("top_info").style["margin"] = "0px";
-    document.getElementById("top_info").innerHTML = "Width : " + myWidth + " Height : " + myHeight
 };
