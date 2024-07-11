778809d2d0837b309af00f712413a43c66c8a833
soueres <bertrand.soueres@gmail.com>
2024-07-11 19:00:43 +0200

    Change size of all 5 buttons !

--- scripts/script_test.js
+++ scripts/script_test.js
@@ -4,7 +4,13 @@ window.onload = displayWindowSize;
 function displayWindowSize() {
     myWidth = window.innerWidth;
     myHeight = window.innerHeight;
-    document.getElementById("btt").style.fontSize = Math.min(Math.max(1000/myWidth, 1), 1.7) + "rem";
     document.getElementById("top_info").innerHTML = "Width : " + myWidth + " Height : " + myHeight;
     document.getElementById("top_info").style["margin"] = "0px";
+    
+    var newFontSize = Math.min(Math.max(1000/myWidth, 1), 1.7);
+    var elements = document.getElementsByClassName("button");
+    
+    for(var i = 0; i < elements.length; ++i) {
+        elements[i].style.fontSize = newFontSize + "rem";
+    }
 };
