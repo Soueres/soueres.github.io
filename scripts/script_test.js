44e877a8f67e0a6ab8bb0d87ab5ae4989b5a3381
soueres <bertrand.soueres@gmail.com>
2024-07-13 15:33:55 +0200

    Approximatively correct behaviour for the navigation pannel
    CSS not finished

--- scripts/script_test.js
+++ scripts/script_test.js
@@ -1,16 +1,21 @@
-window.onresize = displayWindowSize;
-window.onload = displayWindowSize;
+window.onresize = adaptFontSize;
+window.onload = adaptFontSize;
 
-function displayWindowSize() {
+function adaptFontSize() {
     myWidth = window.innerWidth;
     myHeight = window.innerHeight;
     document.getElementById("top_info").innerHTML = "Width : " + myWidth + " Height : " + myHeight;
-    document.getElementById("top_info").style["margin"] = "0px";
     
-    var newFontSize = Math.min(Math.max(1000/myWidth, 1), 1.7);
-    var elements = document.getElementsByClassName("button");
+    /* document.getElementById("top_info").style["margin"] = "0px"; */
+    /* var newFontSize = Math.min(Math.max(1000/myWidth, 1), 1.7); */
     
-    for(var i = 0; i < elements.length; ++i) {
+    /* for(var i = 0; i < elements.length; ++i) {
         elements[i].style.fontSize = newFontSize + "rem";
+    } */
+
+    if( myWidth <= 700 ) {
+        document.getElementById("header-id").style.fontSize = "12px";
+    } else if (myWidth > 700) {
+        document.getElementById("header-id").style.fontSize = "medium";
     }
 };
