b7fb8842f3e39ba1f584d234c5fed0e5c44ec02f
soueres <bertrand.soueres@gmail.com>
2024-07-15 16:39:44 +0200

    Bug corrected in javascript toggle_button

--- scripts/script_DL-buttons.js
+++ scripts/script_DL-buttons.js
@@ -3,7 +3,7 @@ function toggle_on_click() {
     /* console.log(this.id, this.className, "query called : ","#" + this.id + " .toggle-button"); */
     
     const toSwitch = document.querySelector("#" + this.id + " .toggle-button");
-    const toSwitchColor = document.querySelector("#"+this.id);
+    const toSwitchColor = document.querySelector("#" + this.id);
 
     if (toSwitch.style.visibility === "hidden") {
 
@@ -14,9 +14,14 @@ function toggle_on_click() {
 
         toSwitch.style.setProperty('visibility', 'visible');
         toSwitchColor.style.setProperty('background-color', '#d12b2b');
+        
     } else {
+
+        for (let i=1 ; i<=24 ; i++) {
+           document.querySelector("#bt" + i).removeAttribute("style");
+        }
+
         toSwitch.style.setProperty('visibility', 'hidden');
-        toSwitchColor.querySelector("#bt" + i).removeAttribute("style");
     }
 }  
 
