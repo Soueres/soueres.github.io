a6eb96f6d1513b7ccf090054aa06c7544fe8811f
soueres <bertrand.soueres@gmail.com>
2024-07-15 15:41:53 +0200

    Functionnal second page
    Javascript can be improved using the "remove style" function.

--- scripts/script_DL-buttons.js
+++ scripts/script_DL-buttons.js
@@ -1,10 +1,25 @@
-function testfunction(number) {
-    
-    var toSwitch = document.getElementById("b" + number);
+function toggle_on_click() {
 
-    if (toSwitch.style.display === "none") {
-        toSwitch.style.display = "flex";
+    /* console.log(this.id, this.className, "query called : ","#" + this.id + " .toggle-button"); */
+    
+    const toSwitch = document.querySelector("#" + this.id + " .toggle-button");
+    const toSwitchColor = document.querySelector("#"+this.id);
+
+    if (toSwitch.style.visibility === "hidden") {
+
+        for (let i=1 ; i<=24 ; i++) {
+            document.querySelector("#bt" + i + " .toggle-button").style.setProperty('visibility', 'hidden');
+            document.querySelector("#bt" + i).removeAttribute("style");
+        }
+
+        toSwitch.style.setProperty('visibility', 'visible');
+        toSwitchColor.style.setProperty('background-color', '#d12b2b');
     } else {
-        toSwitch.style.display = "none";
+        toSwitch.style.setProperty('visibility', 'hidden');
+        toSwitchColor.querySelector("#bt" + i).removeAttribute("style");
     }
-  } 
+}  
+
+for (let i=1 ; i<=24 ; i++) {
+    document.querySelector("#bt" + i).addEventListener("click", toggle_on_click);
+}
